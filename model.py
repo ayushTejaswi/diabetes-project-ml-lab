be3b70f9be4c4fa168debf0ea4d33b3583f0a2c1
ayushTejaswi <ayushtejaswi03@gmail.com>
2025-09-25 22:36:35 +0530

    Removed unnecessary files and improved the accuracy

--- model.py
+++ model.py
@@ -3,35 +3,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from flask import Flask, request, jsonify
-import numpy as np
-from flask_cors import CORS 
+from flask import Flask, request, jsonify   
+from flask_cors import CORS
 
-# Load the dataset
-df = pd.read_csv('db.csv')
 
-# Perform one-hot encoding
-one_hot_encoded = pd.get_dummies(df['Gender'], prefix='Gender')
-df = pd.concat([df, one_hot_encoded], axis=1)
+df = pd.read_csv('db.csv',index_col = 0)
 
-# Prepare data for modeling
-y = df['Diagnosis']
-x = df.drop(['Diagnosis', 'Gender_f', 'Gender','Unnamed: 0'], axis=1)
-xt, xs, yt, ys = train_test_split(x, y, test_size=0.20, random_state=0)
+one_hot_encoded = pd.get_dummies(df['Gender'],drop_first=True,prefix='Gender')
+df = pd.concat([df,one_hot_encoded],axis = 1)
+df.drop('Gender',inplace=True,axis = 1)
 
-# Create and train the KNN classifier
-k = 5
+X =df.drop('Diagnosis',axis = 1)
+y= df['Diagnosis']
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.3,random_state = 42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+
+
+k = 57
 knn_classifier = KNeighborsClassifier(n_neighbors=k)
-knn_classifier.fit(x.values, y.values)
-#ensembling
+knn_classifier.fit(X_train,y_train)
 
-# Create and train the MLP classifier
-mlp_classifier = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=500, random_state=42)
-mlp_classifier.fit(x.values, y.values)
+mlp_classifier = MLPClassifier(hidden_layer_sizes=(100,), max_iter=1000, random_state=42,activation='tanh',alpha=0.01,solver='adam',learning_rate='adaptive')
+mlp_classifier.fit(X_train, y_train)
 
-# Create Flask app
-app = Flask(__name__)
+feature_names = ["Age", "BMI", "Chol", "TG", "HDL", "LDL", "Cr", "BUN", "Gender_M"]
+
+app= Flask(__name__)
 CORS(app)
 # Define prediction route
 @app.route('/predict', methods=['POST'])
@@ -39,23 +39,31 @@ def predict():
     data = request.get_json()
     if data['Gender']=='M':
         Gender_M=1
-        Gender_F=0
-        
-    if data['Gender']=='F':
+    elif data['Gender']=='F':
         Gender_M=0
-        Gender_F=1
-        
-    # Prepare input features for prediction
-    features = [float(data['Age']), float(data['BMI']),float(data['Chol']),float(data['TG']),float(data['HDL']),float(data['LDL']),float(data['Cr']),float(data['BUN']),Gender_F,Gender_M]  # Add more features as needed
+    
+    features = pd.DataFrame([[
+        float(data["Age"]),
+        float(data["BMI"]),
+        float(data["Chol"]),
+        float(data["TG"]),
+        float(data["HDL"]),
+        float(data["LDL"]),
+        float(data["Cr"]),
+        float(data["BUN"]),
+        Gender_M
+    ]], columns=feature_names)
 
-    # Make predictions using the models
-    knn_prediction = knn_classifier.predict([features])[0]
-    mlp_prediction = mlp_classifier.predict([features])[0]
 
+    features_scaled = scaler.transform(features) 
+
+   
+    knn_prediction = knn_classifier.predict(features_scaled)[0]
+    mlp_prediction = mlp_classifier.predict(features_scaled)[0]
     # Combine predictions using a simple voting mechanism
     ensemble_prediction = int(round((knn_prediction + mlp_prediction) / 2))
 
-    # Map prediction to diagnosis
+   
     diagnosis = 'Yes, I am sorry but you have diabetes' if ensemble_prediction == 1 else 'No, you can relax, you do not have diabetes'
 
     return jsonify({'diagnosis': diagnosis})
--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css'; // Import CSS file for styling
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
   const [features, setFeatures] = useState({
-    'BMI': '',
-    'Age': '',        
-    'Gender': '',
-    'Chol': '',        
-    'TG': '',            
-    'HDL': '',           
-    'LDL': '',           
-    'Cr': '',           
-    'BUN': ''
-    // Add more features as needed
+    BMI: "",
+    Age: "",
+    Chol: "",
+    TG: "",
+    HDL: "",
+    LDL: "",
+    Cr: "",
+    BUN: "",
+    Gender: "M", // default to Male
   });
-  const [diagnosis, setDiagnosis] = useState('');
+
+  const [diagnosis, setDiagnosis] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleInputChange = (event) => {
@@ -27,11 +27,14 @@ function App() {
     event.preventDefault();
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:5000/predict', features);
+      const response = await axios.post(
+        "http://localhost:5000/predict",
+        features
+      );
       setDiagnosis(response.data.diagnosis);
       setLoading(false);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
       setLoading(false);
     }
   };
@@ -40,7 +43,21 @@ function App() {
     <div className="container">
       <h1 className="title">Welcome to IIEST Diagnostic Centre</h1>
       <h2 className="subheading">Please enter the details below:</h2>
+
       <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="Gender">Gender:</label>
+          <select
+            id="Gender"
+            name="Gender"
+            value={features.Gender}
+            onChange={handleInputChange}
+            className="form-control"
+          >
+            <option value="M">Male</option>
+            <option value="F">Female</option>
+          </select>
+        </div>
         <div className="form-group">
           <label htmlFor="Age">Age:</label>
           <input
@@ -48,11 +65,12 @@ function App() {
             className="form-control"
             id="Age"
             name="Age"
-            value={features.Glucose}
+            value={features.Age}
             onChange={handleInputChange}
             required
           />
         </div>
+
         <div className="form-group">
           <label htmlFor="BMI">BMI:</label>
           <input
@@ -65,30 +83,20 @@ function App() {
             required
           />
         </div>
+
         <div className="form-group">
-          <label htmlFor="Gender">Gender:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="Gender"
-            name="Gender"
-            value={features.Glucose}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="Chol">Cholestrol:</label>
+          <label htmlFor="Chol">Cholesterol:</label>
           <input
             type="text"
             className="form-control"
             id="Chol"
             name="Chol"
-            value={features.Glucose}
+            value={features.Chol}
             onChange={handleInputChange}
             required
           />
         </div>
+
         <div className="form-group">
           <label htmlFor="TG">TG:</label>
           <input
@@ -96,7 +104,20 @@ function App() {
             className="form-control"
             id="TG"
             name="TG"
-            value={features.Glucose}
+            value={features.TG}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="HDL">HDL:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="HDL"
+            name="HDL"
+            value={features.HDL}
             onChange={handleInputChange}
             required
           />
@@ -108,23 +129,12 @@ function App() {
             className="form-control"
             id="LDL"
             name="LDL"
-            value={features.Glucose}
+            value={features.LDL}
             onChange={handleInputChange}
             required
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="HDL">HDL:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="HDL"
-            name="HDL"
-            value={features.Glucose}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
+
         <div className="form-group">
           <label htmlFor="Cr">Cr:</label>
           <input
@@ -132,11 +142,12 @@ function App() {
             className="form-control"
             id="Cr"
             name="Cr"
-            value={features.Glucose}
+            value={features.Cr}
             onChange={handleInputChange}
             required
           />
         </div>
+
         <div className="form-group">
           <label htmlFor="BUN">BUN:</label>
           <input
@@ -144,17 +155,28 @@ function App() {
             className="form-control"
             id="BUN"
             name="BUN"
-            value={features.Glucose}
+            value={features.BUN}
             onChange={handleInputChange}
             required
           />
         </div>
-        {/* Add more input fields for additional features */}
+
         <button type="submit" className="btn btn-primary" disabled={loading}>
-          {loading ? 'Predicting...' : 'Predict Diagnosis'}
+          {loading ? "Predicting..." : "Predict Diagnosis"}
         </button>
       </form>
-      {diagnosis && <h2 className={`diagnosis ${diagnosis === "Yes, I am sorry but you have diabetes" ? "green" : "red"}`}>Diagnosis: {diagnosis}</h2>}
+
+      {diagnosis && (
+        <h2
+          className={`diagnosis ${
+            diagnosis === "Yes, I am sorry but you have diabetes"
+              ? "green"
+              : "red"
+          }`}
+        >
+          Diagnosis: {diagnosis}
+        </h2>
+      )}
     </div>
   );
 }
